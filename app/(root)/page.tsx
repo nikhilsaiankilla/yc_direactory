016d558960dc0a1cda6c96009d409f077dd88a9e
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-01-24 19:15:31 +0530

    feat/hero-section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-   <section>Home</section>
+    <section className="pink_container">
+      <h1 className="heading">pitch your startup, <br />connect with entrepreneures</h1>
+      <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and get noticed in virtual competitions</p>
+    </section>
   );
 }
