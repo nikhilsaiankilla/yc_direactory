bf0d001e2232103e7638c4fe7781d26c3a98aff6
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-01-24 22:12:02 +0530

    feat/startup-card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: {
@@ -5,11 +6,33 @@ export default async function Home({ searchParams }: {
 }) {
   const query = (await searchParams).query;
 
+  const posts = [{
+    _id : 1,
+    _createdAt: "yesturday",
+    view: 55,
+    author: { _id: 1, name : "nikhil", image : "https://img.freepik.com/premium-photo/boy-flat-cartoon-character-illustration_620650-2063.jpg" },
+    description: "this is description",
+    image: "https://images.unsplash.com/photo-1735818978089-85970221e678?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Robots",
+    title: "I am Robot"
+  }]
   return (
-    <section className="pink_container">
-      <h1 className="heading">pitch your startup, <br />connect with entrepreneures</h1>
-      <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and get noticed in virtual competitions</p>
-      <SearchForm query={query} />
-    </section>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">pitch your startup, <br />connect with entrepreneures</h1>
+        <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and get noticed in virtual competitions</p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">{query ? `Search results for "${query}"` : "All Startups"}</p>
+
+        <ul className="card_grid">
+          {
+            posts?.length > 0 ? (posts?.map((post: StartupCardType, index: number) => (<StartupCard post={post} key={post?._id}/>))) : (<p className="no-results">No startups Found</p>)
+          }
+        </ul>
+      </section>
+    </>
   );
 }
