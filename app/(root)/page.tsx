e1850ddc442c4753087a0f9430c89fec932060e8
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-01-24 19:46:17 +0530

    feat/search-form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query;
+
   return (
     <section className="pink_container">
       <h1 className="heading">pitch your startup, <br />connect with entrepreneures</h1>
       <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and get noticed in virtual competitions</p>
+      <SearchForm query={query} />
     </section>
   );
 }
