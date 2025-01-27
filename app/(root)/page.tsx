9f8c2c712806de79faba787fcdc535880922ba14
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-01-27 19:32:31 +0530

    feat/user-session-user-db-storeing-implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
@@ -9,6 +10,9 @@ export default async function Home({ searchParams }: {
   const query = (await searchParams).query;
   const params = { search: query || null }
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
+
+  const session = await auth();
+  console.log(session);
 
   return (
     <>
