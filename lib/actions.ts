53649650a641fc5aefa3dc6bbd2e28169a1ce248
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-02-05 12:26:49 +0530

    feat/image-upload

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,22 +4,40 @@ import { auth } from "@/auth";
 import { parseServerActionResponse } from "./utils";
 import slugify from "slugify";
 import { writeClient } from "@/sanity/lib/write-client";
+import cloudinary from "./cloudinary";
 
 export const createPitch = async (state: any, form: FormData, pitch: string) => {
     const session = await auth();
 
     if (!session) return parseServerActionResponse({ error: "Not Signed In", status: "ERROR" })
 
-    const { title, description, category, link } = Object.fromEntries(Array.from(form).filter(([key]) => key !== 'pitch'));
+    const { title, description, category, image } = Object.fromEntries(Array.from(form).filter(([key]) => key !== 'pitch'));
+
 
     const slug = slugify(title as string, { lower: true, strict: true });
 
     try {
+
+        let imageUrl = "";
+
+        if (image instanceof File) {
+            const arrayBuffer = await image.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+
+            const base64Image = `data:${image.type};base64,${buffer.toString("base64")}`;
+
+            const uploadResponse = await cloudinary.v2.uploader.upload(base64Image, {
+                folder: "pitchpoint",
+            });
+
+            imageUrl = uploadResponse?.secure_url;
+        }
+
         const startup = {
             title,
             description,
             category,
-            image: link,
+            image: imageUrl,
             slug: {
                 _type: slug,
                 current: slug
